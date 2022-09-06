08a1023bce95641f8b2e5ff682414be604acc0f6
bubble <bubblegame061@outlook.it>
2022-09-06 10:26:14 +0200

    micro-sistemazioni

--- components/Home/Reviews.vue
+++ components/Home/Reviews.vue
@@ -7,8 +7,73 @@
         <h5 class="max-w-[800px] font-medium text-[18px] text-[#B4B4B4] mt-[33px] leading-[27px]">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</h5>
     
         <div class="mt-[55px]">
-          <h1>SLIDER</h1>
-      </div>
+            <!-- Slider main container -->
+<div class="swiper">
+  <!-- Additional required wrapper -->
+  <div class="swiper-wrapper">
+    <!-- Slides -->
+    <div class="swiper-slide">Slide 1</div>
+    <div class="swiper-slide">Slide 2</div>
+    <div class="swiper-slide">Slide 3</div>
+    
+  </div>
+  <!-- If we need pagination -->
+  <div class="swiper-pagination"></div>
+
+  <!-- If we need navigation buttons -->
+  <div class="swiper-button-prev"></div>
+  <div class="swiper-button-next"></div>
+
+  <!-- If we need scrollbar -->
+  <div class="swiper-scrollbar"></div>
+</div>
+        </div>
       </div>
     </section>
 </template>
+
+
+<script>
+  
+  const swiper = new Swiper('.swiper', {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+</script>
+<script>
+
+
+ export default {
+  head: {
+    script: [
+      {
+        src: "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js",
+      },
+    ],
+    link: [
+        {
+          rel: 'stylesheet', 
+          href: 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css',
+        },
+    ],
+  }
+  // other config goes here
+}
+</script>
--- pages/thankyou.vue
+++ pages/thankyou.vue
@@ -1,5 +1,5 @@
 <template>
-<div class="h-screen flex flex-col justify-center" >
+<div class="container mx-auto h-screen flex flex-col justify-center" >
     <div class="flex flex-col items-center">
         <div>
             <img src="done_icon.png" alt="icon" />
@@ -9,7 +9,7 @@
             <h1 class="text-[50px]">GRAZIE</h1>
         </div>
 
-        <div class="mt-[54px] font-bold text-secondary">
+        <div class=" mt-[54px] font-bold text-secondary">
             <h2 class="text-[36px]">La tua prenotazione è andata a buon fine</h2>
         </div>
     </div>
@@ -19,7 +19,7 @@
         </div>
 
          <div class="w-full flex justify-center mt-[83px]">
-            <button class=" bg-primary rounded-[11px] text-secondary py-4 px-20 font-semibold text-[24px]">TORNA IN HOME PAGE</button>
+            <button class=" bg-primary rounded-[11px] text-secondary py-4 px-20 font-semibold text-[24px]"><h3>TORNA IN HOME PAGE</h3></button>
         </div>
 </div>
 </template>
